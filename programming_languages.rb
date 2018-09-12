ca6afc52f8cd155cfda08784c80875d3ca52ea7f
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 03:18:36 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,44 +7,9 @@ def reformat_languages(languages)
       if reformatted[language].has_key?(:style)
         reformatted[language][:style] << style
       else
-        # binding.pry
         reformatted[language][:style] = [style]
       end
     end
   end
   return reformatted
 end
-
-# languages = {
-#   :oo => {
-#     :ruby => {
-#       :type => "interpreted"
-#     },
-#     :javascript => {
-#       :type => "interpreted"
-#     },
-#     :python => {
-#       :type => "interpreted"
-#     },
-#     :java => {
-#       :type => "compiled"
-#     }
-#   },
-#   :functional => {
-#     :clojure => {
-#       :type => "compiled"
-#     },
-#     :erlang => {
-#       :type => "compiled"
-#     },
-#     :scala => {
-#       :type => "compiled"
-#     },
-#     :javascript => {
-#       :type => "interpreted"
-#     }
-#
-#   }
-# }
-#
-# p reformat_languages(languages)
