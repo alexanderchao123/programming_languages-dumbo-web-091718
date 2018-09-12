5bf1e602ac47641b720b5a1867712e3e4c3a7dd6
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 03:17:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,12 +4,12 @@ def reformat_languages(languages)
   languages.each do |style, langs|
     langs.each do |language, type|
       reformatted[language] = type if !reformatted.has_key?(language)
-      # if reformatted[language].has_key?(:style)
-      #   reformatted[language][:style] << style
-      # else
-      #   # binding.pry
-      #   reformatted[language][:style] = [style]
-      # end
+      if reformatted[language].has_key?(:style)
+        reformatted[language][:style] << style
+      else
+        # binding.pry
+        reformatted[language][:style] = [style]
+      end
     end
   end
   return reformatted
