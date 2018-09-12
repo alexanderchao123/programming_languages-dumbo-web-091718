d9d59f14c5ef5db82f8d30d1475e93a976ad48a7
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 02:25:56 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  reformatted = {}
+
+  languages.each do |style, languages|
+    languages.each do |language, type|
+      reformatted[language] = type
+    end
+  end
+
+  return reformatted
 end
